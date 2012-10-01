35820b6f417a99592e782e243fb142078f7a26b9
Joseph Bass <josephbass@attask.com>
2012-10-01 16:20:44 -0600

    changing the way we do iteration in findAssignments so it's less intensive. It was causing the user profile page to hang in production

--- src/main/java/com/joelj/jenkins/claimblame/BlamerFactory.java
+++ src/main/java/com/joelj/jenkins/claimblame/BlamerFactory.java
@@ -2,7 +2,10 @@ package com.joelj.jenkins.claimblame;
 
 import hudson.model.Job;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.WeakHashMap;
 
 /**
@@ -17,16 +20,23 @@ public class BlamerFactory {
 	private static Map<String, Blamer> blamers = new WeakHashMap<String, Blamer>();
 
 	public static Blamer getBlamerForJob(Job job) {
-		String fullName = job.getFullName();
+		return getBlamerForJob(job.getFullName());
+	}
+
+	public static Blamer getBlamerForJob(String jobName) {
 		Blamer result;
-		if(!blamers.containsKey(fullName)) {
-			FileSystemBlamer fileSystemBlamer = new FileSystemBlamer(fullName);
+		if(!blamers.containsKey(jobName)) {
+			FileSystemBlamer fileSystemBlamer = new FileSystemBlamer(jobName);
 			fileSystemBlamer.load();
-			blamers.put(fullName, fileSystemBlamer);
+			blamers.put(jobName, fileSystemBlamer);
 			result = fileSystemBlamer;
 		} else {
-			result = blamers.get(fullName);
+			result = blamers.get(jobName);
 		}
 		return result;
+	}
+
+	public static Set<String> getTrackedJobs() {
+		return blamers.keySet();
 	}
 }
